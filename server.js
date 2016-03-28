8e773295f872015f72a77811751d9d97c1f87017
Tamm Sjödin <tamm@Mellon.local>
2016-03-28 23:03:49 +0200

    add jade view

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(3000, function () {
