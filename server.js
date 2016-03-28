23fe6dadda279a16076b4e541a90854be270402b
Tamm Sjödin <tamm@Mellon.local>
2016-03-29 00:02:46 +0200

    save the date function with count of days since occurance

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
@@ -5,11 +6,35 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  res.render('index');
+	var latestDate = new Date();
+
+	var log = fs.readFileSync('static/log.txt','utf8');
+    var lines = log.trim().split('\n');
+    var lastLine = lines.slice(-1)[0];
+
+    if (lines.length > 0) {
+    	latestDate = new Date(lastLine);
+    }
+
+    var	timeSinceLastDate = Math.round((new Date().getTime() - latestDate.getTime()) / (1000*60*60*24));
+
+	res.render('index', {
+		'latestDate': latestDate,
+		'timeSinceLastDate': timeSinceLastDate
+	});
+});
+
+app.get('/update', function (req, res) {
+	fs.appendFile('static/log.txt', new Date().toISOString() + '\n', function (err) {
+
+	});
+
+
+	res.redirect('/');
 });
 
 app.use('/static', express.static('static'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+	console.log('Example app listening on port 3000!');
 });
