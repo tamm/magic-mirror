186c2246232c661bb149a6f287944e00ad6f2745
Tamm Sjödin <tamm@Mellon.local>
2016-04-20 22:27:50 +0200

    comment out angular console.log code, trying to avoid ipad error

--- js/angular.js
+++ js/angular.js
@@ -13462,101 +13462,101 @@ function $LogProvider() {
   };
 
   this.$get = ['$window', function($window) {
-    return {
-      /**
-       * @ngdoc method
-       * @name $log#log
-       *
-       * @description
-       * Write a log message
-       */
-      log: consoleLog('log'),
+    // return {
+    //   /**
+    //    * @ngdoc method
+    //    * @name $log#log
+    //    *
+    //    * @description
+    //    * Write a log message
+    //    */
+    //   log: consoleLog('log'),
 
-      /**
-       * @ngdoc method
-       * @name $log#info
-       *
-       * @description
-       * Write an information message
-       */
-      info: consoleLog('info'),
+    //   /**
+    //    * @ngdoc method
+    //    * @name $log#info
+    //    *
+    //    * @description
+    //    * Write an information message
+    //    */
+    //   info: consoleLog('info'),
 
-      /**
-       * @ngdoc method
-       * @name $log#warn
-       *
-       * @description
-       * Write a warning message
-       */
-      warn: consoleLog('warn'),
+    //   /**
+    //    * @ngdoc method
+    //    * @name $log#warn
+    //    *
+    //    * @description
+    //    * Write a warning message
+    //    */
+    //   warn: consoleLog('warn'),
 
-      /**
-       * @ngdoc method
-       * @name $log#error
-       *
-       * @description
-       * Write an error message
-       */
-      error: consoleLog('error'),
+    //   /**
+    //    * @ngdoc method
+    //    * @name $log#error
+    //    *
+    //    * @description
+    //    * Write an error message
+    //    */
+    //   error: consoleLog('error'),
 
-      /**
-       * @ngdoc method
-       * @name $log#debug
-       *
-       * @description
-       * Write a debug message
-       */
-      debug: (function() {
-        var fn = consoleLog('debug');
+    //   /**
+    //    * @ngdoc method
+    //    * @name $log#debug
+    //    *
+    //    * @description
+    //    * Write a debug message
+    //    */
+    //   debug: (function() {
+    //     var fn = consoleLog('debug');
 
-        return function() {
-          if (debug) {
-            fn.apply(self, arguments);
-          }
-        };
-      }())
-    };
+    //     return function() {
+    //       if (debug) {
+    //         fn.apply(self, arguments);
+    //       }
+    //     };
+    //   }())
+    // };
 
-    function formatError(arg) {
-      if (arg instanceof Error) {
-        if (arg.stack) {
-          arg = (arg.message && arg.stack.indexOf(arg.message) === -1)
-              ? 'Error: ' + arg.message + '\n' + arg.stack
-              : arg.stack;
-        } else if (arg.sourceURL) {
-          arg = arg.message + '\n' + arg.sourceURL + ':' + arg.line;
-        }
-      }
-      return arg;
-    }
+    // function formatError(arg) {
+    //   if (arg instanceof Error) {
+    //     if (arg.stack) {
+    //       arg = (arg.message && arg.stack.indexOf(arg.message) === -1)
+    //           ? 'Error: ' + arg.message + '\n' + arg.stack
+    //           : arg.stack;
+    //     } else if (arg.sourceURL) {
+    //       arg = arg.message + '\n' + arg.sourceURL + ':' + arg.line;
+    //     }
+    //   }
+    //   return arg;
+    // }
 
-    function consoleLog(type) {
-      // var console = $window.console || {},
-      //     logFn = console[type] || console.log || noop,
-      //     hasApply = false;
+    // function consoleLog(type) {
+    //   // var console = $window.console || {},
+    //   //     logFn = console[type] || console.log || noop,
+    //   //     hasApply = false;
 
-      // // Note: reading logFn.apply throws an error in IE11 in IE8 document mode.
-      // // The reason behind this is that console.log has type "object" in IE8...
-      // try {
-      //   hasApply = !!logFn.apply;
-      // } catch (e) {}
+    //   // // Note: reading logFn.apply throws an error in IE11 in IE8 document mode.
+    //   // // The reason behind this is that console.log has type "object" in IE8...
+    //   // try {
+    //   //   hasApply = !!logFn.apply;
+    //   // } catch (e) {}
 
-      // if (hasApply) {
-      //   return function() {
-      //     var args = [];
-      //     forEach(arguments, function(arg) {
-      //       args.push(formatError(arg));
-      //     });
-      //     return logFn.apply(console, args);
-      //   };
-      // }
+    //   // if (hasApply) {
+    //   //   return function() {
+    //   //     var args = [];
+    //   //     forEach(arguments, function(arg) {
+    //   //       args.push(formatError(arg));
+    //   //     });
+    //   //     return logFn.apply(console, args);
+    //   //   };
+    //   // }
 
-      // // we are IE which either doesn't have window.console => this is noop and we do nothing,
-      // // or we are IE where console.log doesn't have apply so we log at least first 2 args
-      // return function(arg1, arg2) {
-      //   logFn(arg1, arg2 == null ? '' : arg2);
-      // };
-    }
+    //   // // we are IE which either doesn't have window.console => this is noop and we do nothing,
+    //   // // or we are IE where console.log doesn't have apply so we log at least first 2 args
+    //   // return function(arg1, arg2) {
+    //   //   logFn(arg1, arg2 == null ? '' : arg2);
+    //   // };
+    // }
   }];
 }
 
