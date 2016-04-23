59b52e453d7fa1da433cd93c90c7a1ad02c93b79
Tamm Sjödin <tamm@Mellon.local>
2016-04-23 02:28:54 +0200

    handle multiple date counters

--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
-var app = angular.module('magicMirror', ['ngMaterial']);
+var app = angular.module('magicMirror', []);
 
 app.controller("dayCounter", ["$scope", "$http", function($scope, $http) {
-    $scope.count = 0;
-    $scope.updateDate = function() {
-        $http.get("/update")
-		    .then(function(response) {
-		        $scope.count = response.data.days;
-		        console.log(response.data);
-		    });
-    }
-    $scope.getDays = function() {
-        $http.get("/days")
-		    .then(function(response) {
-		        $scope.count = response.data.days;
-		        console.log(response.data);
-		    });
-    }
+	$scope.counts = [0];
 
-    $scope.getDays();
+	$scope.updateDate = function(dateId) {
+		$http.get("/update/" + dateId)
+			.then(function(response) {
+				for (var i = 0; i < response.data.days.length; i++) {
+					$scope.counts[i] = response.data.days[i];
+				}
+				console.log(response.data);
+			});
+	};
+
+	$scope.addDate = function() {
+		var dateId = $scope.counts.length;
+		$http.get("/update/" + dateId)
+			.then(function(response) {
+				for (var i = 0; i < response.data.days.length; i++) {
+					$scope.counts[i] = response.data.days[i];
+				}
+				console.log(response.data);
+			});
+	};
+
+	$scope.getDays = function() {
+		$http.get("/days")
+			.then(function(response) {
+				for (var i = 0; i < response.data.days.length; i++) {
+					$scope.counts[i] = response.data.days[i];
+				}
+				console.log(response.data);
+			});
+	};
+
+	$scope.getDays();
 }]);
--- server.js
+++ server.js
@@ -11,44 +11,62 @@ app.get('/', function (req, res) {
 	});
 });
 
-app.get('/days', function (req, res) {
-	var latestDate = new Date();
-
+var routeAllDates = function (req, res) {
+	var latestDate = [new Date()];
+	var timeSinceLastDate = [];
+	var log;
 	try {
-		var log = fs.readFileSync('static/log.txt','utf8');
-    	var lines = log.trim().split('\n');
-    	var lastLine = lines.slice(-1)[0];
-	    if (lines.length > 0) {
-	    	latestDate = new Date(lastLine);
-	    }
+		log = fs.readFileSync('static/log.txt','utf8');
 	} catch (error) {
 		console.log('no log file found');
+		res.status(404).send();
 	}
+	var lines = log.trim().split('\n');
+	var days = [];
+	var latestDates = [];
 
-	latestDate.setHours(23,59,0);
-	var now = new Date();
+	if (lines.length > 0) {
+		for (var i = 0; i <= lines.length - 1; i++) {
+			var line = lines[i].split(';;');
+			var dateObject = new Date(line[1]);
+			var dateId = line[0];
+			var dateId = line[0];
+			var now = new Date();
 
-    var	timeSinceLastDate = Math.ceil((now.getTime() - latestDate.getTime()) / (1000*60*60*24));
+			dateObject.setHours(23,59,0);
 
-	res.send({
-		days: timeSinceLastDate,
-		date: latestDate
-	});
+			days[dateId] = Math.ceil((now.getTime() - dateObject.getTime()) / (1000*60*60*24));
+			latestDates[dateId] = dateObject;
+
+		}
+
+		res.send({
+			days: days,
+			date: latestDates
+		});
+	}
+	else {
+		res.status(404).send();
+	}
+};
+
+app.get('/days', routeAllDates);
+
+app.param('id', function(req, res, next, id) {
+  req.id = id;
+  next();
 });
 
-app.get('/update', function (req, res) {
-	var date = new Date().toISOString()
-	fs.appendFile('static/log.txt', date + '\n', function (err) {
+app.get('/update/:id', function (req, res) {
+	var date = new Date().toISOString();
+	var dateString = req.id + ';;' + date + '\n';
+	fs.appendFile('static/log.txt', dateString, function (err) {
 		if (err) {
 			res.status(500).send({error: 'Unable to write to log'});
 		} else {
-			res.send({
-				days: 0,
-				date: date
-			});
+			routeAllDates(req, res);
 		}
 	});
-
 });
 
 app.use('/static', express.static('static'));
